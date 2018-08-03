4ed3ecb5d278c2145494f0636d86c05be882e405
Barry Livingstone <barryliv"hotmail.co.uk>
2018-08-03 14:10:17 -0700

    UAT

---
+++ src/test/java/com/seleniumsimplified/webdriver/FirstTest.java
@@ -0,0 +1,21 @@
+package com.seleniumsimplified.webdriver;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static junit.framework.Assert.assertTrue;
+
+public class FirstTest
+
+{
+    @Test
+    public void driverIsTheKing()
+    {
+        WebDriver driver = new HtmlUnitDriver();
+        driver.get("https://compendiumdev.co.uk/selenium/");
+
+        assertTrue(driver.getTitle().startsWith("Selenium Simplified"));
+    }
+}
